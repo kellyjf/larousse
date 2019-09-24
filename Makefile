a2536ff2afd3ff7fa7376c2e0a40449bf29af355
John Kelly <jkelly@aircell.com>
2019-09-24 17:51:44 -0600

    UI Work

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 
 
 UI := ui_word.py ui_vowels.py ui_usage.py ui_expression.py \
-	ui_encounter.py
+	ui_encounter.py ui_media.py ui_mediaedit.py
 
 ifneq ($(NUM),)
 NOPT := --number $(NUM)
@@ -18,7 +18,8 @@ database:
 	./parse.py $(AUDIO) $(subst words/,,$(wildcard words/*))
 
 test: $(UI)
-	python3 app_encounter.py
+	python3 app_media.py
+	#python3 app_encounter.py
 	#python3 app_word.py
 	#python3 app_expression.py
 	#python3 app_usage.py
---
+++ app_media.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'word.ui'
+#
+# Created by: PyQt5 UI code generator 5.12.1
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+from ui_media import Ui_Media
+import time
+from database import Media, Root
+
+class MediaDialog(QtWidgets.QDialog, Ui_Media):
+	def __init__(self, parent=None):
+		super(QtWidgets.QDialog,self).__init__(parent)
+		self.setupUi(self)
+
+
+if __name__ == "__main__":
+	import sys
+	app = QtWidgets.QApplication(sys.argv)
+	win=MediaDialog()
+	win.show()
+	sys.exit(app.exec_())
