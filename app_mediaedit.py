24d37142c605d8733ad2283e07f1812fc9a898ed
John Kelly <jkelly@aircell.com>
2019-09-24 21:16:03 -0600

    Add new media

--- app_mediaedit.py
+++ app_mediaedit.py
@@ -10,18 +10,44 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 from ui_mediaedit import Ui_MediaEdit
-import time
-from database import Media, Root
+from datetime import date, datetime
+from database import Media, Root, Session
 
 class MediaEditDialog(QtWidgets.QDialog, Ui_MediaEdit):
 	def __init__(self, parent=None):
 		super(QtWidgets.QDialog,self).__init__(parent)
 		self.setupUi(self)
+		self.session=Session()
 
+	def setmedia(self,media):
+		self.media=media
+		self.nameLine.setText(media.name)
+		self.urlLine.setText(media.url)
+		self.createdDate.setDate(QtCore.QDate(media.created.year,media.created.month,media.created.day))
+		self.notesText.setText(media.notes)
+
+	def accept(self):
+		self.media.name=self.nameLine.text()
+		self.media.url=self.urlLine.text()
+		cdate=self.createdDate.date()
+		self.media.created=date(cdate.year(),cdate.month(),cdate.day())
+		self.media.notes=self.notesText.toPlainText()
+
+		self.session.add(self.media)
+		self.session.commit()
+
+		print("accept")
+		super(MediaEditDialog,self).accept()
+
+	def reject(self):
+		print("reject")
+		super(MediaEditDialog,self).reject()
+		
 
 if __name__ == "__main__":
 	import sys
 	app = QtWidgets.QApplication(sys.argv)
 	win=MediaEditDialog()
+	win.setmedia(Media(created=datetime.now()))
 	win.show()
 	sys.exit(app.exec_())
